88398f61cfadd6e1aa7859749e0ce957f9c28b4a
A01411547 <57514414+A01411547@users.noreply.github.com>
2020-12-03 22:08:44 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,58 +6,72 @@
  */
 
 #include <iostream>
+#include <string>
 #include <sstream>
+#include <vector>
 #include <cstring>
 #include <string>
-#include "dlist.h"
-
+#include "sorts.h"
 
 using namespace std;
 
+template <class T>
+string arrayToString(const vector<T> &v) {
+	stringstream aux;
+
+	aux << "[" << v[0];
+	for (int i = 1; i < v.size(); i++) {
+		aux << ", " << v[i];
+	}
+	aux << "]";
+	return aux.str();
+}
+
+
 int main(int argc, char* argv[]) {
-	DList<int> b1;
-	string front, back;
 
-	b1.add(2);
-	front = "[2]";
-	back = "[2]";
-	cout << " 1 " <<	(!front.compare(b1.toStringForward()) ? "success\n" : "fail\n");
-	cout << " 1 " <<	(!back.compare(b1.toStringBackward()) ? "success\n" : "fail\n");
+	int array1[] = {58, 92, 75, 5, 21, 92, 84, 91, 64, 54, 3, 28, 11, 61, 65, 4, 1, 8, 52, 78};
+	vector<int> original1 (array1, array1 + sizeof(array1) / sizeof(int) );
 
- 	b1.add(3);
-	b1.add(6);
-	b1.add(10);
-	front = "[2, 3, 6, 10]";
-	back = "[10, 6, 3, 2]";
-	cout << " 2 " <<	(!front.compare(b1.toStringForward()) ? "success\n" : "fail\n");
-	cout << " 2 " <<	(!back.compare(b1.toStringBackward()) ? "success\n" : "fail\n");
+	int array2[] = {120, 10, 5, 897, 333, 68};
+	vector<int> original2 (array2, array2 + sizeof(array2) / sizeof(int) );
 
-	cout << " 3 " <<	(3 == b1.find(10) ? "success\n" : "fail\n");
+	vector<int> prueba;
+	Sorts <int> sorts;
+	string resp1 = "[1, 3, 4, 5, 8, 11, 21, 28, 52, 54, 58, 61, 64, 65, 75, 78, 84, 91, 92, 92]";
+	string resp2 = "[5, 10, 68, 120, 333, 897]";
 
-	cout << " 4 " <<	(-1 == b1.find(1) ? "success\n" : "fail\n");
+	prueba = original1;
+	sorts.ordenaSeleccion(prueba);
 
-	b1.update(1,13);
-	front = "[2, 13, 6, 10]";
-	back = "[10, 6, 13, 2]";
-	cout << " 5 " <<	(!front.compare(b1.toStringForward()) ? "success\n" : "fail\n");
-	cout << " 5 " <<	(!back.compare(b1.toStringBackward()) ? "success\n" : "fail\n");
+	cout << " 1 " <<	(!resp1.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
 
-	b1.update(3,32);
-	front = "[2, 13, 6, 32]";
-	back = "[32, 6, 13, 2]";
-	cout << " 6 " <<	(!front.compare(b1.toStringForward()) ? "success\n" : "fail\n");
-	cout << " 6 " <<	(!back.compare(b1.toStringBackward()) ? "success\n" : "fail\n");
+	prueba = original2;
+	sorts.ordenaSeleccion(prueba);
+	cout << " 2 " <<	(!resp2.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
 
-	b1.remove(0);
-	front = "[13, 6, 32]";
-	back = "[32, 6, 13]";
-	cout << " 7 " <<	(!front.compare(b1.toStringForward()) ? "success\n" : "fail\n");
-	cout << " 7 " <<	(!back.compare(b1.toStringBackward()) ? "success\n" : "fail\n");
+	prueba = original1;
+	sorts.ordenaBurbuja(prueba);
+	cout << " 3 " <<	(!resp1.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
 
-	b1.remove(1);
-	front = "[13, 32]";
-	back = "[32, 13]";
-	cout << " 8 " <<	(!front.compare(b1.toStringForward()) ? "success\n" : "fail\n");
-	cout << " 8 " <<	(!back.compare(b1.toStringBackward()) ? "success\n" : "fail\n");
+	prueba = original2;
+	sorts.ordenaBurbuja(prueba);
+	cout << " 4 " <<	(!resp2.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
 
+	prueba = original1;
+	sorts.ordenaMerge(prueba);
+	cout << " 5 " <<	(!resp1.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
+
+	prueba = original2;
+	sorts.ordenaMerge(prueba);
+	cout << " 6 " <<	(!resp2.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
+
+	sorts.ordenaMerge(prueba);
+  cout << " 7 " <<	(5 == sorts.busqSecuencial(prueba, 897) ? "success\n" : "fail\n");
+
+	cout << " 8 " <<	(-1 == sorts.busqSecuencial(prueba, 100) ? "success\n" : "fail\n");
+
+	cout << " 9 " <<	(4 == sorts.busqBinaria(prueba, 333) ? "success\n" : "fail\n");
+
+	cout << " 10" <<	(3 == sorts.busqBinaria(prueba, 197) ? "success\n" : "fail\n");
 }
